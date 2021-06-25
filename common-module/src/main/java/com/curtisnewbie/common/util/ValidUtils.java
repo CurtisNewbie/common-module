6f010c09c36f10dcaaf686d0c3fb5e31e26728c3
CurtisNewbie <zhuangyongj@gmail.com>
2021-06-26 00:07:44 +0800

    Add extra validation methods

--- common-module/src/main/java/com/curtisnewbie/common/util/ValidUtils.java
+++ common-module/src/main/java/com/curtisnewbie/common/util/ValidUtils.java
@@ -2,6 +2,7 @@ package com.curtisnewbie.common.util;
 
 
 import com.curtisnewbie.common.exceptions.MsgEmbeddedException;
+import org.springframework.util.StringUtils;
 
 import java.util.Objects;
 
@@ -14,15 +15,25 @@ import java.util.Objects;
  * @author yongjie.zhuang
  */
 public final class ValidUtils {
+    private static final String DEFAULT_NON_NULL_MSG = "Please enter required content";
+    private static final String DEFAULT_STRING_NOT_EMPTY_MSG = "It should not be empty";
 
     private ValidUtils() {
 
     }
 
-    public static <T> void requireNonNull(T t) throws MsgEmbeddedException {
-        if (t == null) {
-            throw new MsgEmbeddedException("Please enter required content");
+    public static void requireNotEmpty(String text) throws MsgEmbeddedException {
+        requireNotEmpty(text, DEFAULT_STRING_NOT_EMPTY_MSG);
+    }
+
+    public static void requireNotEmpty(String text, String errMsg) throws MsgEmbeddedException {
+        if (!StringUtils.hasText(text)) {
+            throw new MsgEmbeddedException(errMsg);
         }
+    }
+
+    public static <T> void requireNonNull(T t) throws MsgEmbeddedException {
+        requireNonNull(t, DEFAULT_NON_NULL_MSG);
     }
 
     public static <T> void requireNonNull(T t, String errMsg) throws MsgEmbeddedException {
@@ -33,6 +44,12 @@ public final class ValidUtils {
 
     public static <T, V> void requireEquals(T t, V v, String errMsg) throws MsgEmbeddedException {
         if (!Objects.equals(t, v)) {
+            throw new MsgEmbeddedException(errMsg);
+        }
+    }
+
+    public static <T, V> void requireNotEquals(T t, V v, String errMsg) throws MsgEmbeddedException {
+        if (Objects.equals(t, v)) {
             throw new MsgEmbeddedException(errMsg);
         }
     }
