f4259b40343ef3dfab211db9f6fae3b2259921a9
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-19 20:37:49 +0800

    Improve JsonUtils

--- common-module/src/main/java/com/curtisnewbie/common/util/JsonUtils.java
+++ common-module/src/main/java/com/curtisnewbie/common/util/JsonUtils.java
@@ -1,7 +1,10 @@
 package com.curtisnewbie.common.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+
+import java.util.TimeZone;
 
 /**
  * Utils for JSON processing
@@ -10,7 +13,14 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
  */
 public final class JsonUtils {
 
-    private static final JsonMapper jsonMapper = new JsonMapper();
+    private static final JsonMapper jsonMapper;
+
+    static {
+        JsonMapper jm = new JsonMapper();
+        jm.setTimeZone(TimeZone.getDefault());
+        jm.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        jsonMapper = jm;
+    }
 
     private JsonUtils() {
 
@@ -23,6 +33,18 @@ public final class JsonUtils {
      */
     public static String writeValueAsString(Object o) throws JsonProcessingException {
         return jsonMapper.writeValueAsString(o);
+    }
+
+    /**
+     * Read value as object using internally cached {@link JsonMapper}
+     *
+     * @param json
+     * @param clz
+     * @param <T>
+     * @throws JsonProcessingException
+     */
+    public static <T> T readValueAsObject(String json, Class<T> clz) throws JsonProcessingException {
+        return jsonMapper.readValue(json, clz);
     }
 
 }
