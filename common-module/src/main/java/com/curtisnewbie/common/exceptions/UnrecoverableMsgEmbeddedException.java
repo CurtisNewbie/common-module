66ae1338a27d7003b7dc54e340ca017866c8110e
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-31 18:22:34 +0800

    Add UnrecoverableMsgEmbeddedException

--- common-module/src/main/java/com/curtisnewbie/common/exceptions/UnrecoverableMsgEmbeddedException.java
+++ common-module/src/main/java/com/curtisnewbie/common/exceptions/UnrecoverableMsgEmbeddedException.java
@@ -2,16 +2,11 @@ package com.curtisnewbie.common.exceptions;
 
 
 /**
- * <p>
- * Exception with message embedded that is <b>recoverable</b>
- * </p>
- * <p>
- * This is a checked exception, <b>it won't cause transaction rollback!</b>
- * </p>
+ * Exception with message embedded that is not recoverable
  *
  * @author yongjie.zhuang
  */
-public class MsgEmbeddedException extends Exception {
+public class UnrecoverableMsgEmbeddedException extends RuntimeException {
 
     /**
      * Custom Message for this exception
@@ -21,7 +16,7 @@ public class MsgEmbeddedException extends Exception {
     /**
      * @param msg message that can be both shown in stacktrace and later retrieved by getter/setter
      */
-    public MsgEmbeddedException(String msg) {
+    public UnrecoverableMsgEmbeddedException(String msg) {
         super(msg);
         this.msg = msg;
     }
@@ -30,7 +25,7 @@ public class MsgEmbeddedException extends Exception {
      * @param msg message that can be both shown in stacktrace and later retrieved by getter/setter
      * @param e   cause
      */
-    public MsgEmbeddedException(String msg, Throwable e) {
+    public UnrecoverableMsgEmbeddedException(String msg, Throwable e) {
         super(msg, e);
         this.msg = msg;
     }
@@ -40,12 +35,12 @@ public class MsgEmbeddedException extends Exception {
      * @param customMsg       custom message that can be retrieved by getter/setter
      * @param e               cause
      */
-    public MsgEmbeddedException(String msgInStacktrace, String customMsg, Throwable e) {
+    public UnrecoverableMsgEmbeddedException(String msgInStacktrace, String customMsg, Throwable e) {
         super(msgInStacktrace, e);
         this.msg = customMsg;
     }
 
-    public MsgEmbeddedException() {
+    public UnrecoverableMsgEmbeddedException() {
 
     }
 
