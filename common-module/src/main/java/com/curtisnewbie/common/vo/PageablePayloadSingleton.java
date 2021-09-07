87244e6c439be62c88e5201634b856c45f88358f
CurtisNewbie <zhuangyongj@gmail.com>
2021-09-07 14:27:49 +0800

    Add PageablePayloadSingleton

---
+++ common-module/src/main/java/com/curtisnewbie/common/vo/PageablePayloadSingleton.java
@@ -0,0 +1,20 @@
+package com.curtisnewbie.common.vo;
+
+import lombok.Data;
+
+/**
+ * <p>
+ * A pageable value object with a single payload object
+ * </p>
+ * <p>
+ * Convenient payload wrapper that needs {@link PagingVo}
+ * </p>
+ *
+ * @author yongjie.zhuang
+ */
+@Data
+public class PageablePayloadSingleton<T> extends PageableVo {
+
+    private T payload;
+
+}
