6bf88aabf0f704c3e8314d9ac466b2acbdc7acc3
yongj.zhuang <yongj.zhuang@outlook.com>
2022-02-21 21:03:11 +0800

    More on MapperUtils

--- common-module/src/main/java/com/curtisnewbie/common/util/MapperUtils.java
+++ common-module/src/main/java/com/curtisnewbie/common/util/MapperUtils.java
@@ -1,7 +1,12 @@
 package com.curtisnewbie.common.util;
 
+import com.baomidou.mybatisplus.core.conditions.*;
 import com.baomidou.mybatisplus.core.conditions.query.*;
 import com.baomidou.mybatisplus.core.mapper.*;
+import com.curtisnewbie.common.dao.*;
+import org.springframework.util.*;
+
+import java.util.function.*;
 
 /**
  * Utils for Mapper
@@ -19,5 +24,30 @@ public final class MapperUtils {
         final QueryWrapper<T> w = new QueryWrapper<T>()
                 .eq(column, value);
         return baseMapper.selectOne(w);
+    }
+
+    /**
+     * Select one and convert it, if the selected record is null, a null value is returned directly without conversion
+     */
+    public static <T, V> V selectAndConvert(Wrapper<T> wrapper, BaseMapper<T> baseMapper, Function<T, V> converter) {
+        Assert.notNull(wrapper, "wrapper == null");
+        Assert.notNull(converter, "converter == null");
+        T t = baseMapper.selectOne(wrapper);
+        if (t == null)
+            return null;
+        return converter.apply(t);
+    }
+
+    /**
+     * Select a record and get id from it
+     */
+    public static <T> Integer selectAndMapId(Wrapper<DaoSkeleton> wrapper, BaseMapper<DaoSkeleton> baseMapper) {
+        Assert.notNull(wrapper, "wrapper == null");
+
+        DaoSkeleton t = baseMapper.selectOne(wrapper);
+        if (t == null)
+            return null;
+
+        return t.getId();
     }
 }
