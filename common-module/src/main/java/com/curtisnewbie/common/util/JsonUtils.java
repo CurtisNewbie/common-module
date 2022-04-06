c7c7f884dd6a8b550973cbbfb9a12bf47fb0efa2
yongj.zhuang <yongj.zhuang@outlook.com>
2022-04-06 10:55:38 +0800

    Modified: common-module/src/main/java/com/curtisnewbie/common/util/JsonUtils.java

--- common-module/src/main/java/com/curtisnewbie/common/util/JsonUtils.java
+++ common-module/src/main/java/com/curtisnewbie/common/util/JsonUtils.java
@@ -1,11 +1,12 @@
 package com.curtisnewbie.common.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.*;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
-import java.util.TimeZone;
+import java.util.*;
 
 /**
  * Utils for JSON processing
@@ -22,8 +23,6 @@ public final class JsonUtils {
 
     /**
      * Write value as String using internally cached {@link JsonMapper}
-     *
-     * @throws JsonProcessingException
      */
     public static String writeValueAsString(Object o) throws JsonProcessingException {
         return jsonMapper.writeValueAsString(o);
@@ -31,14 +30,17 @@ public final class JsonUtils {
 
     /**
      * Read value as object using internally cached {@link JsonMapper}
-     *
-     * @param json
-     * @param clz
-     * @param <T>
-     * @throws JsonProcessingException
      */
     public static <T> T readValueAsObject(String json, Class<T> clz) throws JsonProcessingException {
         return jsonMapper.readValue(json, clz);
+    }
+
+    /**
+     * Read value as List using internally cached {@link JsonMapper}
+     */
+    public static <T> List<T> readValueAsList(String json, Class<T> clz) throws JsonProcessingException {
+        return jsonMapper.readValue(json, new TypeReference<List<T>>() {
+        });
     }
 
     /**
@@ -48,6 +50,9 @@ public final class JsonUtils {
         return jsonMapper.constructType(clz);
     }
 
+    /**
+     * Construct a new Json Mapper
+     */
     public static JsonMapper constructsJsonMapper() {
         JsonMapper jm = new JsonMapper();
         jm.setTimeZone(TimeZone.getDefault());
