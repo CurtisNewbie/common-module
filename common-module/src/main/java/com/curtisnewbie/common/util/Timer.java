ec6964eae162536d1b262497debb77099c14b150
yongj.zhuang <yongj.zhuang@outlook.com>
2022-04-10 21:41:40 +0800

    Timer

---
+++ common-module/src/main/java/com/curtisnewbie/common/util/Timer.java
@@ -0,0 +1,54 @@
+package com.curtisnewbie.common.util;
+
+
+import org.springframework.util.*;
+
+import java.time.*;
+
+/**
+ * Timer based on {@link LocalDateTime}
+ * <p>
+ * It's <b>not</b> thread-Safe
+ * </p>
+ *
+ * @author yongj.zhuang
+ */
+public class Timer {
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    /** Get a new timer and start it */
+    public static Timer startTimer() {
+        Timer t = new Timer();
+        t.start();
+        return t;
+    }
+
+    /** start the timer */
+    private void start() {
+        Assert.isNull(startTime, "Timer started already");
+        startTime = LocalDateTime.now();
+    }
+
+    /** stop the timer */
+    public void stop() {
+        Assert.isNull(endTime, "Timer stopped already");
+        endTime = LocalDateTime.now();
+    }
+
+    /** Get duration */
+    public Duration duration() {
+        Assert.notNull(startTime, "Timer hasn't started yet");
+        Assert.notNull(endTime, "Timer hasn't stopped yet");
+        return Duration.between(startTime, endTime);
+    }
+
+    /** Print Duration */
+    public String printDuration() {
+        final Duration duration = duration();
+        final String s = duration.toString().substring(2);
+        return s.replace("H", " Hours ").replace("M", " Minutes ").replace("S", " Seconds ");
+    }
+
+}
