c8125049697439ac2518314a3c77d4cf095777e8
yongj.zhuang <yongj.zhuang@outlook.com>
2022-04-11 17:31:56 +0800

    Chained Map

---
+++ common-module/src/main/java/com/curtisnewbie/common/data/ChainedMap.java
@@ -0,0 +1,36 @@
+package com.curtisnewbie.common.data;
+
+import java.util.*;
+
+/**
+ * Chained Map
+ *
+ * @author yongj.zhuang
+ */
+public class ChainedMap {
+
+    private Map<String, String> param;
+
+    public ChainedMap(Map<String, String> param) {
+        this.param = new HashMap<>(param);
+    }
+
+    public ChainedMap() {
+        this.param = new HashMap<>();
+    }
+
+    /**
+     * Put value for key
+     */
+    public ChainedMap thenPut(String k, String v) {
+        param.put(k, v);
+        return this;
+    }
+
+    /**
+     * Get the internal Map
+     */
+    public Map<String, String> get() {
+        return param;
+    }
+}
