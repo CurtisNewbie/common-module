74167b24aa251df8b5687599bcb90d9984b84743
yongj.zhuang <zhuangyongj@gmail.com>
2022-05-24 00:15:56 +0800

    Handle Validation Exception

--- common-module/src/main/java/com/curtisnewbie/common/advice/GlobalControllerAdvice.java
+++ common-module/src/main/java/com/curtisnewbie/common/advice/GlobalControllerAdvice.java
@@ -2,11 +2,20 @@ package com.curtisnewbie.common.advice;
 
 import com.curtisnewbie.common.exceptions.MsgEmbeddedException;
 import com.curtisnewbie.common.exceptions.UnrecoverableException;
+import com.curtisnewbie.common.util.StructUtils;
 import com.curtisnewbie.common.vo.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.bind.validation.BindValidationException;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+import static java.lang.String.format;
 
 /**
  * Base Controller Advice
@@ -41,6 +50,21 @@ public class GlobalControllerAdvice {
             errorMsg = "Invalid request";
         }
         return Result.error(errorMsg, e.getErrorCode());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseBody
+    public Result<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final BindingResult bindingResult = e.getBindingResult();
+        final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        if (fieldErrors.isEmpty()) {
+            return Result.error("Invalid Argument");
+        }
+
+        // find the first error
+        final FieldError fieldError = fieldErrors.get(0);
+        return Result.error(format("%s %s", fieldError.getField(), fieldError.getDefaultMessage()));
     }
 
 }
