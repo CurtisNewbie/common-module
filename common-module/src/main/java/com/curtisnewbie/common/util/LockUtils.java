523326b7c0deb9d5bb171bf9e2e33256095436fa
yongj.zhuang <zhuangyongj@gmail.com>
2022-08-25 00:30:20 +0800

    LockUtils but for Callable

--- common-module/src/main/java/com/curtisnewbie/common/util/LockUtils.java
+++ common-module/src/main/java/com/curtisnewbie/common/util/LockUtils.java
@@ -2,6 +2,7 @@ package com.curtisnewbie.common.util;
 
 import org.springframework.util.*;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.locks.*;
 
 /**
@@ -14,6 +15,24 @@ public final class LockUtils {
     private LockUtils() {
 
     }
+
+    /**
+     * Lock, call Callable of V, and then return V
+     */
+    public static <V> V lockAndCall(Lock lock, Callable<V> r) {
+        Assert.notNull(lock, "lock == null");
+        Assert.notNull(r, "callable == null");
+
+        lock.lock(); // if this throws exception, we didn't get the lock at all, then we don't need to unlock
+        try {
+            return r.call();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
 
     /**
      * Lock then run
