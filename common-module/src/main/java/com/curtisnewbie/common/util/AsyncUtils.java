e193050489ac1aba7e4e1bb38b2ae3ce293585ef
yongj.zhuang <yongj.zhuang@outlook.com>
2022-08-27 16:22:11 +0800

    Impl: Tracing in AsyncUtils

--- common-module/src/main/java/com/curtisnewbie/common/util/AsyncUtils.java
+++ common-module/src/main/java/com/curtisnewbie/common/util/AsyncUtils.java
@@ -1,34 +1,64 @@
 package com.curtisnewbie.common.util;
 
+import brave.*;
 import com.curtisnewbie.common.vo.*;
+import lombok.extern.slf4j.*;
+import org.springframework.beans.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.*;
+import org.springframework.stereotype.*;
 import org.springframework.web.context.request.async.*;
 
 import java.util.concurrent.*;
 import java.util.function.*;
 
 /**
- * Async Utils
+ * Async Utils (Tracer aware, do not try to instantiate this bean)
  *
  * @author yongj.zhuang
  */
-public final class AsyncUtils {
+@Slf4j
+@Component
+public class AsyncUtils implements ApplicationContextAware {
 
-    private AsyncUtils() {
+    private static volatile Tracer tracer = null;
+
+    public static DeferredResult<Result<Void>> runAsync(Runnable r) {
+        return runAsyncResult(() -> {
+            r.run();
+            return null;
+        });
     }
 
-    public static <V> DeferredResult<Result<V>> ofAsyncResult(Supplier<V> supplier) {
+    public static <V> DeferredResult<Result<V>> runAsyncResult(Supplier<V> supplier) {
         return runAsync(() -> Result.of(supplier.get()));
     }
 
     public static <V> DeferredResult<V> runAsync(Supplier<V> supplier) {
         final DeferredResult<V> dr = new DeferredResult<>();
+        final Span span = tracer != null ? tracer.currentSpan() : null;
 
         CompletableFuture.runAsync(() -> {
-            dr.setErrorResult(supplier.get());
+            if (span != null) {
+                try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+                    dr.setErrorResult(supplier.get());
+                }
+            } else {
+                dr.setErrorResult(supplier.get());
+            }
         }).exceptionally(e -> {
             dr.setErrorResult(e);
             return null;
         });
         return dr;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        try {
+            AsyncUtils.tracer = applicationContext.getBean(Tracer.class);
+        } catch (Exception e) {
+            log.error("Failed to obtain Tracer from ApplicationContext", e);
+        }
     }
 }
