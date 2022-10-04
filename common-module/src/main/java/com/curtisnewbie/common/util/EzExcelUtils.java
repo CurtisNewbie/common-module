2d878d194d1f53e3e4dba67a34314e8cb28ecbfe
yongj.zhuang <yongj.zhuang@outlook.com>
2022-10-04 15:44:55 +0800

    Easy Excel and more on Runner

---
+++ common-module/src/main/java/com/curtisnewbie/common/util/EzExcelUtils.java
@@ -0,0 +1,65 @@
+package com.curtisnewbie.common.util;
+
+import com.alibaba.excel.*;
+import com.alibaba.excel.write.metadata.*;
+import org.springframework.util.*;
+
+import javax.servlet.http.*;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.*;
+import java.util.*;
+
+import static com.curtisnewbie.common.util.Runner.*;
+
+/**
+ * Utils for alibaba Easy Excel
+ *
+ * @author yongj.zhuang
+ */
+public final class EzExcelUtils {
+
+    private EzExcelUtils() {
+    }
+
+    /**
+     * Resolve the path to file under 'resource' folder
+     */
+    public static String resolveResourcePath(String resourcePath) {
+        final URL url = EzExcelUtils.class.getClassLoader().getResource(resourcePath);
+        Assert.notNull(url, String.format("File not found, resource path: %s", resourcePath));
+        return url.getPath();
+    }
+
+    /**
+     * Generate excel based on template
+     *
+     * @param params       list of params (for rows)
+     * @param outputStream outputStream
+     * @param templatePath path to template file
+     */
+    public static void writeExcel(List<?> params, OutputStream outputStream, String templatePath) {
+        final ExcelWriter excelWriter = EasyExcel.write(new BufferedOutputStream(outputStream)).withTemplate(templatePath).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet().build();
+        excelWriter.fill(params, writeSheet);
+        excelWriter.finish();
+    }
+
+    /**
+     * Convenience method to set response header for exporting excel file
+     *
+     * @param response servlet response
+     * @param filename filename
+     * @param isCn     whether the filename contains chinese characters
+     */
+    public static void setExcelResponseHeaders(HttpServletResponse response, String filename, boolean isCn) {
+        response.setContentType("application/excel");
+        response.setCharacterEncoding("utf-8");
+        final String fileName = tryCall(() -> URLEncoder.encode(filename, "utf-8"));
+        if (isCn)
+            response.setHeader("Content-disposition", "attachment; filename=" + new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1));
+        else
+            response.setHeader("Content-disposition", "attachment; filename=" + filename);
+    }
+}
+
--- common-module/src/main/java/com/curtisnewbie/common/util/Runner.java
+++ common-module/src/main/java/com/curtisnewbie/common/util/Runner.java
@@ -18,7 +18,7 @@ public final class Runner {
     }
 
     /**
-     * Run the Runnable safely
+     * Run the Runnable safely, exception is handled by the consumer or simply muted
      */
     public static void runSafely(final ThrowableRunnable r, final Consumer<Exception> consumer) {
         if (r == null)
@@ -40,15 +40,15 @@ public final class Runner {
     }
 
     /**
-     * Call if condition returns true
+     * Run if condition returns true, null is considered as false
      */
-    public static void callIf(Callable callable, Supplier<Boolean> condition) throws Exception {
+    public static void callIf(ThrowableRunnable runnable, Supplier<Boolean> condition) throws Exception {
         Assert.notNull(condition, "condition == null");
-        Assert.notNull(callable, "condition == null");
+        Assert.notNull(runnable, "runnable == null");
 
         Boolean cond = condition.get();
         if (cond != null && cond)
-            callable.call();
+            runnable.run();
     }
 
     /**
@@ -61,6 +61,35 @@ public final class Runner {
         Boolean cond = condition.get();
         if (cond != null && cond)
             runnable.run();
+    }
+
+    /**
+     * Try call the Callable, and return the result
+     * <p>
+     * When exception is thrown, this method will wrap the caught exception with an IllegalStateException, and rethrow it
+     * <p>
+     * If the Callable itself is null, null is returned
+     */
+    public static <T> T tryCall(Callable<T> c) {
+        if (c == null) return null;
+        try {
+            return c.call();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /**
+     * Run the runnable
+     * <p>
+     * When exception is thrown, this method will wrap the caught exception with an IllegalStateException, and rethrow it
+     */
+    public static void tryRun(ThrowableRunnable r) {
+        try {
+            r.run();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 
 }
