526127d3a13f2b75210c1efbbfeffdb8851e505d
yongj.zhuang <zhuangyongj@gmail.com>
2022-11-09 22:52:25 +0800

    Fix ddl example

--- common-module/scripts/DaoSkeleton.sql
+++ common-module/scripts/DaoSkeleton.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS [] (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT "primary key",
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT "primary key",
 
-    ...
+  ...
 
-    create_time DATETIME NOT NULL DEFAULT NOW() COMMENT 'when the record is created',
-    create_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
-    update_time DATETIME NOT NULL DEFAULT NOW() COMMENT 'when the record is updated',
-    update_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
-    is_del TINYINT NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted'
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  create_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  update_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  is_del TINYINT NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted'
 ) ENGINE=InnoDB COMMENT '[]';
