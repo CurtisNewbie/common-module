edd136fcd90b9af742351118f82dfb72eb40327f
yongj.zhuang <zhuangyongj@gmail.com>
2022-11-27 17:43:57 +0800

    Adapters for logging respnose object

--- common-module/src/main/java/com/curtisnewbie/common/advice/ControllerConsoleLogAdvice.java
+++ common-module/src/main/java/com/curtisnewbie/common/advice/ControllerConsoleLogAdvice.java
@@ -1,14 +1,28 @@
 package com.curtisnewbie.common.advice;
 
+import com.curtisnewbie.common.data.BiContainer;
 import com.curtisnewbie.common.util.JsonUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.formula.functions.T;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 
 /**
@@ -22,6 +36,36 @@ import org.springframework.util.StopWatch;
 @Slf4j
 @Aspect
 public class ControllerConsoleLogAdvice {
+
+    private static final List<BiContainer<Class, PrintAdapter>> printAdapters = new ArrayList<>();
+
+    static {
+        printAdapters.add(new BiContainer<>(byte[].class, (o) -> ((byte[]) o).length + " bytes"));
+        printAdapters.add(new BiContainer<>(ResponseEntity.class, o -> {
+            ResponseEntity respEntity = ((ResponseEntity) o);
+            StringBuilder sb = new StringBuilder("@ResponseEntity{ ");
+            sb.append("statusCode: ").append(respEntity.getStatusCode()).append(", ");
+            sb.append("body: ");
+
+            Object body = respEntity.getBody();
+            if (body == null) {
+                sb.append("null");
+            } else {
+                sb.append(print(respEntity.getBody()));
+            }
+            sb.append(" }");
+            return sb.toString();
+        }));
+        printAdapters.add(new BiContainer<>(InputStream.class, Object::toString));
+        printAdapters.add(new BiContainer<>(OutputStream.class, Object::toString));
+        printAdapters.add(new BiContainer<>(ServletRequest.class, Object::toString));
+        printAdapters.add(new BiContainer<>(ServletResponse.class, Object::toString));
+        printAdapters.add(new BiContainer<>(DeferredResult.class, Object::toString));
+        printAdapters.add(new BiContainer<>(Runnable.class, Object::toString));
+        printAdapters.add(new BiContainer<>(Callable.class, Object::toString));
+        printAdapters.add(new BiContainer<>(Future.class, Object::toString));
+        printAdapters.add(new BiContainer<>(StreamingResponseBody.class, Object::toString));
+    }
 
     @Pointcut("within(@org.springframework.stereotype.Controller *) || within(@org.springframework.web.bind.annotation.RestController *)")
     public void controllerPointcut() {
@@ -37,17 +81,12 @@ public class ControllerConsoleLogAdvice {
             return result;
         } finally {
             sw.stop();
-            log.info("Pointcut '{}',\n args: '{}',\n took '{}' milliseconds,\n result: '{}'\n",
-                    pjp.toShortString(),
-                    cvtToStr(pjp.getArgs()),
-                    sw.getTotalTimeMillis(),
-                    respToStr(result));
+            log.info("Pointcut '{}',\n args: '{}',\n took '{}' milliseconds,\n result: '{}'\n", pjp.toShortString(), cvtToStr(pjp.getArgs()), sw.getTotalTimeMillis(), print(result));
         }
     }
 
     private static String cvtToStr(Object[] args) {
-        if (args == null)
-            return "[ null ]";
+        if (args == null) return "[ null ]";
 
         StringBuilder sb = new StringBuilder("[ ");
         for (Object o : args) {
@@ -57,48 +96,29 @@ public class ControllerConsoleLogAdvice {
         return sb.toString();
     }
 
-    private static String respToStr(Object o) {
-        if (o == null)
-            return "null";
+    private static void append(StringBuilder sb, String text) {
+        // 2 is the length of ", "
+        if (sb.length() > 2) sb.append(", ");
+        sb.append("'").append(text).append("'");
+    }
 
-        if (o instanceof ResponseEntity) {
-            ResponseEntity respEntity = (ResponseEntity) o;
-            StringBuilder sb = new StringBuilder("@ResponseEntity{ ");
-            sb.append("statusCode: ").append(respEntity.getStatusCode()).append(", ");
-            sb.append("body: ");
-
-            Object body = respEntity.getBody();
-            if (body == null) {
-                sb.append("null");
-            } else {
-                if (body instanceof byte[]) {
-                    sb.append(((byte[]) respEntity.getBody()).length).append(" bytes");
-                } else {
-                    String bodyAsStr;
-                    try {
-                        bodyAsStr = JsonUtils.writePretty(body);
-                    } catch (JsonProcessingException e) {
-                        bodyAsStr = body.toString();
-                    }
-                    sb.append(bodyAsStr);
-                }
+    private static <T> String print(T t) {
+        if (t == null) return "null";
+        for (BiContainer<Class, PrintAdapter> printAdapter : printAdapters) {
+            if (printAdapter.getLeft().isAssignableFrom(t.getClass())) {
+                return printAdapter.getRight().print(t);
             }
-            sb.append(" }");
-            return sb.toString();
-        } else {
-            try {
-                return JsonUtils.writePretty(o);
-            } catch (JsonProcessingException e) {
-                return o.toString();
-            }
+        }
+        try {
+            return JsonUtils.writePretty(t);
+        } catch (JsonProcessingException e) {
+            return t.toString();
         }
     }
 
-    private static void append(StringBuilder sb, String text) {
-        // 2 is the length of ", "
-        if (sb.length() > 2)
-            sb.append(", ");
+    @FunctionalInterface
+    public interface PrintAdapter {
 
-        sb.append("'").append(text).append("'");
+        String print(Object t);
     }
 }
