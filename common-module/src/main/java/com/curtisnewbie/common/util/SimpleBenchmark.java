4a6450b0dd1e723e42faa32fbbecee4963bace45
yongj.zhuang <yongj.zhuang@outlook.com>
2023-02-21 11:14:08 +0800

    Better output formatting

--- common-module/src/main/java/com/curtisnewbie/common/util/SimpleBenchmark.java
+++ common-module/src/main/java/com/curtisnewbie/common/util/SimpleBenchmark.java
@@ -7,13 +7,11 @@ package com.curtisnewbie.common.util;
  */
 public final class SimpleBenchmark {
 
-    public static long PRE_HEAT_ITERATIONS = 30;
-
     private SimpleBenchmark() {}
 
     public static void benchmark(boolean preheat, String name, long iterations, Runnable r) {
         // preheat
-        if (preheat) for (long i = 0; i < PRE_HEAT_ITERATIONS; i++) r.run();
+        if (preheat) for (long i = 0; i < 30; i++) r.run();
 
         final long start = System.currentTimeMillis();
         for (long i = 0; i < iterations; i++) r.run();
@@ -21,9 +19,9 @@ public final class SimpleBenchmark {
         final long spent = (end - start);
         final double eachMilli = (double) spent / (double) iterations;
         if (name != null)
-            System.out.printf("%s: iterations %,d, total spent %,dms, %,d op/s\n", name, iterations, spent, (long) (1000d / eachMilli));
+            System.out.printf("%-30s iterations: %,d  total spent: %,5dms %,16d op/s\n", name, iterations, spent, (long) (1000d / eachMilli));
         else
-            System.out.printf("iterations %,d, total spent %,dms, %,d op/s\n", iterations, spent, (long) (1000d / eachMilli));
+            System.out.printf("iterations: %,d  total spent: %,5dms %,16d op/s\n", iterations, spent, (long) (1000d / eachMilli));
     }
 
     public static void benchmark(long iterations, Runnable r) {
